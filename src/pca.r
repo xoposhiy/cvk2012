a43a8429f8120308783ef17efbe35ed03854a1eb
Mikhail Dvorkin <mikhail.dvorkin@gmail.com>
2012-09-30 18:17:28 +0400

    PCA calculation code and results

---
+++ src/pca.r
@@ -0,0 +1,24 @@
+require(maptools)
+
+data <- read.csv("../data/vectors.csv", encoding="UTF-8")
+#svg("../results/pca.svg")
+pdf("../results/pca.pdf")
+#png("../results/pca.png")
+
+colors = c("green", "red",  "blue", "darkred")
+textcolors = c("black", colors[2:4])
+
+pca <- princomp(data[,3:27])
+x <- pca$scores[,1]
+y <- pca$scores[,2]
+names <- data[,1]
+curias <- data[,2] + 1
+
+write.csv(cbind(as.character(names),curias,x,y), file="../results/pca.csv")
+
+plot(x, y, pch=20, asp=1, col=colors[curias])
+set.seed(123)
+pointLabel(x, y, labels = names, cex=0.45, col=textcolors[curias])
+#text(x, y, names, cex=0.5, pos=3, col="")
+
+dev.off()
