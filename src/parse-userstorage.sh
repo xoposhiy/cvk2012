03cda58f53f8f33a05e85c8b184ed8f3492f4f6f
Pavel Egorov <xoposhiy@mail.ru>
2012-10-03 01:04:30 +0600

    join electors and candidates answers in one file and one plot. Additional component visualization.

--- src/parse-userstorage.sh
+++
@@ -1,2 +0,0 @@
-echo q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,q11,q12,q13,q14,q15,q16,q17,q18,q19,q20,q21,q22,q23,q24,q25 > ../data/user-answers.csv
-grep "" ../data/userstorage.json | perl -pe 's/^.*\[(.*)\].*$/$1/' | grep -v "null" >> ../data/user-answers.csv
---
+++ src/pca-all.r
@@ -0,0 +1,32 @@
+require(maptools)
+
+data <- read.csv("../data/all-answers.csv", encoding="UTF-8")
+users = 7338
+
+pdf("../results/pca_all.pdf")
+colors = c("green", "red",  "blue", "darkred", "grey")
+pchars = c('o', 'o', 'o', 'o', '.')
+textcolors = c("black", colors[2:5])
+
+pca <- princomp(data[,3:27])
+ux <- pca$scores[1:users,1]
+uy <- pca$scores[1:users,2]
+uz <- pca$scores[1:users,3]
+
+candidates = (users+1):length(data[,1])
+
+x <- pca$scores[candidates,1]
+y <- pca$scores[candidates,2]
+z <- pca$scores[candidates,3]
+names <- data[candidates,1]
+curias <- data[candidates,2] + 1
+
+write.csv(pca$loadings, file="../results/pca_all_components.csv")
+
+plot(ux, uy, pch='·', asp=1, col="grey", xlab="Economical freedom", ylab="Tolerance to current goverment")
+points(x, y, pch=20, asp=1, col=colors[curias])
+pointLabel(x, y, labels = names, cex=0.45, col=textcolors[curias])
+plot(ux, uz, pch='·', asp=1, col="grey", xlab="Economical freedom", ylab="RobinGoodness")
+points(x, z, pch=20, asp=1, col=colors[curias])
+pointLabel(x, z, labels = names, cex=0.45, col=textcolors[curias])
+dev.off()
