58f7156a13f1315589abe7ff19d59ad5eccc20e7
Pavel Egorov <xoposhiy@mail.ru>
2012-10-06 23:15:08 +0600

    remove trash from prev-prev commit

--- src/winners.sh
+++
@@ -1,5 +0,0 @@
-for i in 1 2 3 4 
-do
-	
-	curl "http://cvk2012.org/debaty/"$i"_den_kvalifikacionnogo_tura/" | perl -pe "chomp" | perl -ne 's/\s+/ /g; for $i (/<b>([^<]*)<\/b>/g) {print $i."\n\r"; }'
-done
